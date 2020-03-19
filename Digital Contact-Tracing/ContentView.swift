091363b8f64be6696faa8ee545e0cb231f2e45a5
TechSolomon <2001solomon@gmail.com>
2020-03-18 23:42:06 -0800

    Added background color, static text, and selection picker

--- Digital Contact-Tracing/ContentView.swift
+++ Digital Contact-Tracing/ContentView.swift
@@ -9,13 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pickerSelectedItem = 0
+    
     var body: some View {
-        Text("Digital Contact Tracing")
-    }
-}
+        ZStack {
+            
+            Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Digital Contact Tracing")
+                    .font(.system(size: 34))
+                    .fontWeight(.heavy)
+                
+                Picker(selection: $pickerSelectedItem, label: Text("")) {
+                    Text("Left")
+                    Text("Center")
+                    Text("Right")
+                }.pickerStyle(SegmentedPickerStyle())
+            }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        }
     }
 }
